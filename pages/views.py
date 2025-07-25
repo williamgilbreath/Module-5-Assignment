d527681691d378284b8e619ba1475cd5f9f80138
williamgilbreath <wgilbr1@lsu.edu>
2025-07-25 16:45:38 -0500

    fixing changes in about page

--- pages/views.py
+++ pages/views.py
@@ -5,5 +5,5 @@ class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class AboutPageView(TemplateView):
-    template_namse = 'about.html'
+    template_name = 'about.html'
 # Create your views here.
